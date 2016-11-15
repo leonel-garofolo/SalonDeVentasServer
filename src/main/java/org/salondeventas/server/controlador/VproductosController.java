8ac9a9840426499dd0262c597b481c91c9bf114b
lgarofolo <lgarofolo@ROADM117.wiener-lab.net>
2016-11-15 08:32:16 -0300

    arreglos

--- src/main/java/org/salondeventas/server/controlador/VproductosController.java
+++
@@ -1,9 +0,0 @@
-package org.salondeventas.server.controlador;
-
-import java.util.List;
-
-import org.salondeventas.server.modelo.jpa.VproductosEntity;
-
-public interface VproductosController {
-	public List<VproductosEntity> loadAll(String usuario, String clave);
-}
